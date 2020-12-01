5f08eb429dd24b9a00cf364c4d40333b8d8475b9
Vishrut8898 <vishrut9869@gmail.com>
2020-12-01 18:54:27 +0530

    please run

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const nodemailer = require('nodemailer');
 // All Authors Route
 router.get('/', async (req, res) => {
     const services = await Service.find();
-    res.render('index', {services: services, show: false,});
+    res.render('index');
 })
 
 module.exports = router;
